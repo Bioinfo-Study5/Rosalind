58c2fe8e7a85198ce3f089f736c36003bcd27b6d
CHOIsunhyeon <bioinfo0813@gmail.com>
2021-07-15 20:48:37 +0900

    first

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 #! /usr/bin/python
+a = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+print(a.count("A"))
+print(a.count("T"))
+print(a.count("C"))
+print(a.count("G"))
 
-print("Hello World")
