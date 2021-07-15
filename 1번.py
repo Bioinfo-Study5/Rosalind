84ec48ba00844a367ac6afaab50f5f6c7c513a11
CHOIsunhyeon <bioinfo0813@gmail.com>
2021-07-15 20:52:37 +0900

    add 1번.py

---
+++ 1번.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+a = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+print(a.count("A"))
+print(a.count("T"))
+print(a.count("C"))
+print(a.count("G"))
+
