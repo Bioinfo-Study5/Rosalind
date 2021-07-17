7f70749516260073277b64f7ed5fb774f81d2572
CHOIsunhyeon <bioinfo0813@gmail.com>
2021-07-17 13:59:03 +0900

    add 2번문제

---
+++ 2번.py
@@ -0,0 +1,3 @@
+#! /usr/bin/python
+a = "GATGGAACTTGACTACGTAAATT"
+print(a.replace("T","U"))
--- hello.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/python
-a = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
-print(a.count("A"))
-print(a.count("T"))
-print(a.count("C"))
-print(a.count("G"))
-
